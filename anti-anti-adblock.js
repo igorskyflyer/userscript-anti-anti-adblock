e6ceb95a26bac9bc3a843187481be7a4f16b2f51
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-11-28 04:02:16 +0100

    Create anti-anti-adblock.js

---
+++ anti-anti-adblock.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Anti anti-AdBlock
+// @namespace      https://github.com/igorskyflyer
+// @description    Counters anti-AdBlock scripts
+// @include        *
+// @runat          document-idle
+// ==/UserScript==
+
+window.googleAd = true;
