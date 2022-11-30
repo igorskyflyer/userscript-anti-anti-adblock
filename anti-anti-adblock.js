6da70f1f4c5b1647d38b89092859457aae37b791
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-11-30 02:11:38 +0100

    Update anti-anti-adblock.js

--- anti-anti-adblock.js
+++ anti-anti-adblock.js
@@ -2,8 +2,9 @@
 // @name           Anti anti-AdBlock
 // @namespace      https://github.com/igorskyflyer
 // @description    Counters anti-AdBlock scripts
+// @grant          unsafeWindow
 // @include        *
 // @runat          document-idle
 // ==/UserScript==
 
-window.googleAd = true;
+unsafeWindow.googleAd = unsafeWindow.zfgloadedpopup = true;
