b48847a541a126891e044f58288fc468c22aa46a
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-12-10 17:52:11 +0100

    Update anti-anti-adblock.user.js

--- anti-anti-adblock.user.js
+++ anti-anti-adblock.user.js
@@ -14,3 +14,8 @@ unsafeWindow.googleAd =
 unsafeWindow.isLoadAds =
 unsafeWindow.generatorAds =
 unsafeWindow.zfgloadedpopup = true;
+
+unsafeWindow.GeneratorAds =
+unsafeWindow.Ads_PushPage
+unsafeWindow.Ads_Popunder
+unsafeWindow.Ads_Vignette = function() {}
