b1679a2f3b0662be4af4460c241cc71facc27085
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-12-10 18:11:01 +0100

    Fix broken matching pattern
    Fixes #5

--- anti-anti-adblock.user.js
+++ anti-anti-adblock.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/igorskyflyer
 // @description    Counters anti-AdBlock scripts
 // @grant          unsafeWindow
-// @match          *
+// @match          *://*/*
 // @run-at         document-idle
 // @version        1.0.1
 // ==/UserScript==
